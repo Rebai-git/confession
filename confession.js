91e4de0fe36dc26f7ca8ba10236d1606a5aa71a2
Rebai-git <kaungzinthu11423@gmail.com>
2025-08-11 21:37:28 +0900

    Update confession.js

--- confession.js
+++ confession.js
@@ -59,7 +59,7 @@ const lines3 = [
   "I really really love you!"
 ];
 const lines4 = [
-  "I really want to do a lot of thing for you..."
+  "I really want to do a lot of things for you..."
 ];
 const lines5 = [
   "I really want to listen to your yappings!"
@@ -75,7 +75,7 @@ const lines8  = [
 ];
 const lines9  = [
   "but instead,because of my overthinking and my lack of social skills...", "I've made thing awkward, annoying and put even more emotional burden on you",
-  "I feel really bad about it and really sorry about that"
+  "I'm really sorry about that"
 ];
 const lines10 = [
   "Now, I want to know how you feel about me and all of this. And..."
@@ -84,7 +84,7 @@ const lines11 = [
   "If you'd love to give me a chance, I would love to fix all my mistakes and...", "I will try my best to be the person you deserve"
 ];
 const lines12 = [
-  "Of course, it is obviously fine if you don't feel the same", " I'd still love to be your friend if you're okay with it", " After all, it would be my loss to let go of someone as amazing as you!!"
+  "Of course, it is obviously fine if you don't feel the same", " I'd still love to have platonic relationship if you're okay with it", " After all, it would be my loss to let go of someone as amazing as you!!"
 ];
 const lines13 = [
   "Thanks for reading till the end."
